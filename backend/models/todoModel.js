ba88e8a54b7ea7d415d34dafbfe16e613eb9d3b1
MBadarr <mbadar418@gmail.com>
2024-03-19 01:52:32 +0500

    todo schema changes

--- backend/models/todoModel.js
+++ backend/models/todoModel.js
@@ -4,7 +4,7 @@ const todoSchema = new mongoose.Schema({
 
     title: {
         type: String,
-        required: [true, "Please enter the Todo"]
+        required: [true, "Please enter the Title"]
     },
     description: {
         type: String,
