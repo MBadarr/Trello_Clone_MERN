d66bce18bbb17e8ebd639223e67fa43769d7759a
M.Badar <mbadar418@gmail.com>
2024-08-19 11:21:12 +0500

    ShadcnUI Setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
-import './App.css'
+// import { Button } from "@/components/ui/button"
 
 function App() {
 
-  return (
-    <>
-      <h1 className='text-3xl text-center font-bold underline'>Trello Clone</h1>
-    </>
-  )
+    return (
+        <>
+            <h1 className='text-3xl text-center font-bold underline'>Trello Clone</h1>
+            {/*<Button>Click me</Button>*/}
+        </>
+    )
 }
 
 export default App
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
@@ -9,4 +10,9 @@ export default defineConfig({
     },
   },
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
