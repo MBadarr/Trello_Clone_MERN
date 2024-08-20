2b86eb049fb70efa8768a2fd8cadface18dd8e7d
M.Badar <mbadar418@gmail.com>
2024-08-20 09:35:48 +0500

    Home Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,19 @@
-import {useState} from "react";
+import { useState } from "react";
 import { Button } from "@/components/ui/button"
 
 function App() {
-  const [count, setCount] = useState<number>(0)
+    const [count, setCount] = useState<number>(0)
 
-    function Increment() {
+    function handleIncrement() {
         setCount((prevCount: number) => prevCount + 1)
     }
 
-  return (
-    <>
-        <Button onClick={Increment} variant='outline'>Click me</Button>
-        {count}
-    </>
-  )
+    return (
+        <>
+            <Button onClick={handleIncrement} variant='outline'>Click me</Button>
+            {count}
+        </>
+    )
 }
 
 export default App
