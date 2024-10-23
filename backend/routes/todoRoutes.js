23dd877cda2f49cd6e86b1834fde2db2aa924a42
M.Badar <44946634+MBadarr@users.noreply.github.com>
2024-10-23 22:10:08 +0500

    Update todoRoutes.js

--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 const { getAllTasks, createTask, updateTask, deleteTask, getTask } = require('../controllers/todoController');
 
-
 const router = express.Router();
 
 const verifyToken = require("../middleware/verifyToken");
